49bdd58b88a27409ee44380b8ad7173e8dc5f986
Dan E <danelagin@gmail.com>
2018-05-16 20:39:53 +0300

    test

--- app/src/main/java/android1/android1project/MainActivity.java
+++ app/src/main/java/android1/android1project/MainActivity.java
@@ -1,7 +1,11 @@
 package android1.android1project;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView greeting = (TextView)  findViewById(R.id.greeting);
+
+        greeting.setText(greetPhrase());
+
+
+
+
+    }
+    private String greetPhrase(){
+
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        Resources resources = getResources();
+        String username = resources.getString(R.string.user_name);
+        if (currentHour <=12 && currentHour >5  ){
+            return String.format("%s %s!",resources.getString(R.string.morning), username);
+        }
+        else if (currentHour <=5 && currentHour >= 0 ){
+            return String.format("%s %s!",resources.getString(R.string.night), username);
+        }
+        else if (currentHour <=24 && currentHour >= 18 ){
+            return String.format("%s %s!",resources.getString(R.string.evening), username);
+        }
+        else {return String.format("%s %s!",resources.getString(R.string.day), username);}
     }
 }
