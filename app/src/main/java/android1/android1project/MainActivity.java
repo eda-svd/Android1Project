8f16c50ab6c372eb4fcbf2ba2b793ec89540989d
Dan E <danelagin@gmail.com>
2018-05-17 18:51:51 +0300

    test

--- app/src/main/java/android1/android1project/MainActivity.java
+++ app/src/main/java/android1/android1project/MainActivity.java
@@ -18,30 +18,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView weather = (TextView)  findViewById(R.id.weather);
         weather.setText(currentWeather());
-
-
-
-
     }
-    /*private String greetPhrase(){
-
-        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-        Resources resources = getResources();
-        String username = resources.getString(R.string.user_name);
-        if (currentHour <=12 && currentHour >5  ){
-            return String.format("%s %s!",resources.getString(R.string.morning), username);
-        }
-        else if (currentHour <=5 && currentHour >= 0 ){
-            return String.format("%s %s!",resources.getString(R.string.night), username);
-        }
-        else if (currentHour <=24 && currentHour >= 18 ){
-            return String.format("%s %s!",resources.getString(R.string.evening), username);
-        }
-        else {return String.format("%s %s!",resources.getString(R.string.day), username);}
-    }*/
 
     public void refreshButton(View view){
-        Toast.makeText(this,"wat",Toast.LENGTH_SHORT).show();
+        TextView weather = (TextView)  findViewById(R.id.weather);
+        weather.setText(currentWeather());
     }
 
     private String currentWeather(){
@@ -50,6 +31,6 @@ public class MainActivity extends AppCompatActivity {
         String username = resources.getString(R.string.user_name);
         Random rnd = new Random(System.currentTimeMillis());
         int temperature = -68 + rnd.nextInt(128);
-        return String.format("%s %s!",resources.getString(R.string.day), username, temperature, currentHour);
+        return username +"\n"+ "Текущая температура:" + temperature +"градусов";
     }
 }
