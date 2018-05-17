a8d617a298be1588f791b969e5815a8092a1f37c
Dan E <danelagin@gmail.com>
2018-05-17 17:52:22 +0300

    test

--- app/src/main/java/android1/android1project/MainActivity.java
+++ app/src/main/java/android1/android1project/MainActivity.java
@@ -3,9 +3,12 @@ package android1.android1project;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView greeting = (TextView)  findViewById(R.id.greeting);
-
-        greeting.setText(greetPhrase());
+        TextView weather = (TextView)  findViewById(R.id.weather);
+        weather.setText(currentWeather());
 
 
 
 
     }
-    private String greetPhrase(){
+    /*private String greetPhrase(){
 
         int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         Resources resources = getResources();
@@ -36,5 +38,18 @@ public class MainActivity extends AppCompatActivity {
             return String.format("%s %s!",resources.getString(R.string.evening), username);
         }
         else {return String.format("%s %s!",resources.getString(R.string.day), username);}
+    }*/
+
+    public void refreshButton(View view){
+        Toast.makeText(this,"wat",Toast.LENGTH_SHORT).show();
+    }
+
+    private String currentWeather(){
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        Resources resources = getResources();
+        String username = resources.getString(R.string.user_name);
+        Random rnd = new Random(System.currentTimeMillis());
+        int temperature = -68 + rnd.nextInt(128);
+        return String.format("%s %s!",resources.getString(R.string.day), username, temperature, currentHour);
     }
 }
