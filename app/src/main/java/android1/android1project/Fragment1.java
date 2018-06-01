d57af6c8b02f84e39aa9fdce28bf6a05852ee731
Dan E <danelagin@gmail.com>
2018-06-01 14:35:47 +0300

    test

---
+++ app/src/main/java/android1/android1project/Fragment1.java
@@ -0,0 +1,18 @@
+package android1.android1project;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment1,container,false);
+        return rootView;
+    }
+}
